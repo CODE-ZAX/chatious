ee40ada80bc6956b58423de2f760338241b7aa36
CODE-ZAX <faraxshams@gmail.com>
2023-05-23 16:59:18 +0500

    Setup Envoirnement

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// server/server.js
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Chatious Server Running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
